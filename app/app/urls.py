2bc8f699de477c7494ee407f196db3a20eec202b
g0sie <malgorzataderkacz3@gmail.com>
2023-07-28 20:00:02 +0200

    fix: linting - imported but unused

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,9 +18,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
