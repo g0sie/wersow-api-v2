316e2fb24b4b643a25c461f5055f3e9d0e4ca9b4
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2023-08-19 17:04:57 +0200

    fix: pytube error while accessing title of video

--- app/manage.py
+++ app/manage.py
@@ -3,10 +3,14 @@
 import os
 import sys
 
+import pytube
+
+pytube.innertube._default_clients["ANDROID"] = pytube.innertube._default_clients["WEB"]
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
